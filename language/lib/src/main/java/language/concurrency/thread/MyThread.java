bdfad45680059eb008a90ef272874088aca44349
jdhall01 <91612546+jdhall01@users.noreply.github.com>
2021-11-12 10:41:16 +0530

    Java multithreading: Thread.start() and Thread.run()

--- language/lib/src/main/java/language/concurrency/thread/MyThread.java
+++ language/lib/src/main/java/language/concurrency/thread/MyThread.java
@@ -1,18 +1,29 @@
 package language.concurrency.thread;
 
 public class MyThread extends Thread {
+	
+	private String name;
+	
+	public MyThread(String name) {
+		this.name = name;
+	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		System.out.println("running MyThread.run()...");
+		System.out.println(name + ": running MyThread.run()..." + Thread.currentThread().getId());
+		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Thread t = new MyThread();
-		t.run();
+		Thread tStart = new MyThread("START");
+		tStart.start();//start() starts a new thread
+		Thread tRun = new MyThread("RUN");
+		tRun.run();//run() runs the run() method in the current thread
+		
+		System.out.println("running main()..." + Thread.currentThread().getId());
 	}
 
 }
